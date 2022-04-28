75ec83ce44f0cab401f9617ffd6637f21ca713b5
guilhermemartins1999 <201802243@estudantes.ips.pt>
2022-04-28 22:18:43 +0100

    Account Type Selection Page (layout e codigo de passagem da Start Page para esta)

--- ProjetoCM/app/src/main/java/com/example/projetocm/StartPageActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/StartPageActivity.java
@@ -2,7 +2,10 @@ package com.example.projetocm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPageActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class StartPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_page);
+
+        Button signUpButton = (Button) findViewById(R.id.signupbtn);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartPageActivity.this, AccountTypeActivity.class));
+                finish();
+            }
+        });
     }
 }
