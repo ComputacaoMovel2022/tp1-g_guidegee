b4f1fabe719f1d078cc03974c042f3281e7e3a75
guilhermemartins1999 <201802243@estudantes.ips.pt>
2022-05-07 18:04:02 +0100

    Splashscreen (ainda por completar -> mudar o background para branco)

--- ProjetoCM/app/src/main/java/com/example/projetocm/StartPageActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/StartPageActivity.java
@@ -11,7 +11,19 @@ public class StartPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashTheme);
+
+        //apenas para teste pois a app inicia demasiado rapido
+        //TODO: mudar a cor do background da splashscreen para branco (ta preto em light mode)
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         super.onCreate(savedInstanceState);
+
+        setTheme(R.style.Theme_ProjetoCM);
         setContentView(R.layout.start_page);
 
         if (getSupportActionBar() != null)
