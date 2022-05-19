11ac5f9057caa691be253cb4b883bc678021c583
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2022-05-19 16:06:31 +0100

    removed action bar

--- ProjetoCM/app/src/main/java/com/example/projetocm/MainActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/MainActivity.java
@@ -16,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.refugee_history);
 
+        if (getSupportActionBar() != null)
+        {
+            getSupportActionBar().hide();
+        }
 
         simpleList = findViewById(R.id.refugeeHistoryList);
         System.out.println("GOT HERE");
