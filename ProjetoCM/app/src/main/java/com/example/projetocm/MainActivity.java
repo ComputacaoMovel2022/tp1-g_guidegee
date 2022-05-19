712e58af0a16dba3fa3159050c395143005ad262
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2022-05-19 15:28:28 +0100

    Added listing profiles feature

--- ProjetoCM/app/src/main/java/com/example/projetocm/MainActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.projetocm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] profilePictures = {R.drawable.test_profile_pic, R.drawable.empty_profile_icon, R.drawable.empty_profile_icon, R.drawable.empty_profile_icon, R.drawable.empty_profile_icon, R.drawable.empty_profile_icon, R.drawable.empty_profile_icon, R.drawable.empty_profile_icon};
+    String[] profileNames = {"Mariana Gustavo", "Perfil Vazio", "Perfil Vazio", "Perfil Vazio", "Perfil Vazio", "Perfil Vazio", "Perfil Vazio", "Perfil Vazio"};
+    ListView simpleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_requests_from_refugees);
+        setContentView(R.layout.refugee_history);
+
+
+        simpleList = findViewById(R.id.refugeeHistoryList);
+        System.out.println("GOT HERE");
+        SimplifiedProfileItem simplifiedProfileItem = new SimplifiedProfileItem(getApplicationContext(), R.layout.refugee_history_list_element, profileNames, profilePictures);
+        simpleList.setAdapter(simplifiedProfileItem);
+
     }
 }
