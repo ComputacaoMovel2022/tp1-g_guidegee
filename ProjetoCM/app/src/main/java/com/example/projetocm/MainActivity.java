9cc876e4e0ed8449c092febc739296609c9f52c1
LuisFonseca-cyber <80035086+LuisFonseca-cyber@users.noreply.github.com>
2022-05-24 14:18:37 +0100

    Update and Review

--- ProjetoCM/app/src/main/java/com/example/projetocm/MainActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.projetocm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.guide_history);
+        Button reviewGuideButton = (Button) findViewById(R.id.buttonReview1);
+        reviewGuideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent itRatingGuide = new Intent(MainActivity.this,RatingGuide.class);
+                itRatingGuide.putExtra("guideName","Ricardo");
+                startActivity(itRatingGuide);
+            }
+        });
+
     }
+
+//    public void openRatingPage(View v){
+//        Intent itRatingGuide = new Intent(this,RatingGuide.class);
+//        itRatingGuide.putExtra("guideName","Ricardo");
+//        startActivity(itRatingGuide);
+//    }
+
+    
 }
---
+++ ProjetoCM/app/src/main/java/com/example/projetocm/RatingGuide.java
@@ -0,0 +1,55 @@
+package com.example.projetocm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+
+public class RatingGuide extends AppCompatActivity {
+    //RatingBar simpleRatingBar = (RatingBar) findViewById(R.id.simpleRatingBar);
+    //Button submitButton = (Button) findViewById(R.id.submitButton);
+    String guideName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rating_guide);
+        getSupportActionBar().hide();
+
+        Intent it = getIntent();
+        guideName = it.getStringExtra("guideName");
+
+
+        // initiate rating bar and a button
+        final RatingBar simpleRatingBar = (RatingBar) findViewById(R.id.simpleRatingBar);
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        Button backButton = (Button) findViewById(R.id.buttonBack);
+        // perform click event on button
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float ratingValue = simpleRatingBar.getRating();
+
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent guideHistory = new Intent(RatingGuide.this,MainActivity.class);
+                startActivity(guideHistory);
+            }
+        });
+
+
+
+    }
+
+    //getRating()
+    //getNumStars()
+
+
+}
