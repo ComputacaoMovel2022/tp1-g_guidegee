d31a37b72ceb8799c8380a37bfb13acdc1f77fce
Rafael <200221055@estudantes.ips.pt>
2022-05-25 18:15:02 +0100

    Start of java

--- ProjetoCM/app/src/main/java/com/example/projetocm/EditProfile.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/EditProfile.java
@@ -3,12 +3,35 @@ package com.example.projetocm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Compare Token
+        //if equal add edit page
+        //else if
         setContentView(R.layout.activity_edit_profile);
+        Button saveChanges= (Button)findViewById(R.id.SaveEdits);
+        saveChanges.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Save
+            }
+        });
+        Button cancelChanges= (Button)findViewById(R.id.CancelEdits);
+        cancelChanges.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Cancel
+            }
+        });
     }
 }
--- ProjetoCM/app/src/main/java/com/example/projetocm/Settings.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/Settings.java
@@ -3,6 +3,14 @@ package com.example.projetocm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +18,111 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        /**
+         * Delete Account
+         */
+        Button deleteAccount= (Button)findViewById(R.id.DeleteAccount);
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Delete Account
+                //maybe a pop-up to be sure
+            }
+        });
+
+        /**
+         * Activate/Deactivate Notification
+         */
+        Switch notificationSwitch = (Switch)  findViewById(R.id.Notification);
+        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //Deactivate Notifications
+                }else{
+                    //Activate Notifications
+                }
+            }
+
+        });
+
+        /**
+         * Activate/Deactivate Availability
+         */
+        Switch availabilitySwitch = (Switch)  findViewById(R.id.AvailabilityChoice);
+        availabilitySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //Activate availability
+                }else{
+                    //Deactivate availability
+                }
+            }
+
+        });
+
+        /**
+         * choose the Distance
+         */
+        Spinner distanceChoiceSpinner = (Spinner)  findViewById(R.id.DistanceChoice);
+        distanceChoiceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Change the distance in DB
+            }
+            //bugs without this part
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        /**
+         * choose the Unity
+         */
+        Spinner unityChoiceSpinner = (Spinner)  findViewById(R.id.UnityChoice);
+        unityChoiceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Change the distance in DB, kilometer(KM) or miles(mi)
+            }
+            //bugs without this part
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        /**
+         * choose the Language
+         */
+        Spinner languageChoiceSpinner = (Spinner)  findViewById(R.id.LanguageChoice);
+        languageChoiceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Change the xml string file
+            }
+            //bugs without this part
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
+
+        /**
+         * Activate/Deactivate Darkmode
+         */
+        Switch darkModeSwitch = (Switch)  findViewById(R.id.DarkMode);
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //DarkMode activated
+                }else{
+                    //DarkMode Deactivated
+                }
+            }
+
+        });
     }
 }
