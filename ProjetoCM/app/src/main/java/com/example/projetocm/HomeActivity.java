2ef4f1af86b6c8175a0a77116db327dafcd3cff1
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2022-05-26 17:34:06 +0100

    asdadasd

--- ProjetoCM/app/src/main/java/com/example/projetocm/HomeActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.projetocm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void clickProfile(View view) {
+        startActivity(new Intent(HomeActivity.this, EditProfile.class));
+        finish();
     }
 }
