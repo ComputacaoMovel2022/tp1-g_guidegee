9cdad8d5ce539808514603ffe0155a47eedefe52
Rafael <200221055@estudantes.ips.pt>
2022-05-28 21:08:57 +0100

    Choose Language

--- ProjetoCM/app/src/main/java/com/example/projetocm/Settings.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/Settings.java
@@ -2,9 +2,17 @@ package com.example.projetocm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -20,9 +28,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.rpc.context.AttributeContext;
+
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.util.Locale;
 
 public class Settings extends AppCompatActivity {
 
@@ -132,6 +144,16 @@ public class Settings extends AppCompatActivity {
         languageChoiceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 //Change the xml string file
+                switch (i){
+                    case 0:
+                        setLocale(getApplicationContext(), "en");
+                        recreate();
+                        break;
+                    case 1:
+                        setLocale(getApplicationContext(), "pt");
+                        recreate();
+                        break;
+                }
             }
             //bugs without this part
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -155,5 +177,42 @@ public class Settings extends AppCompatActivity {
             }
 
         });
+    }
+
+    private static final String SELECTED_LANGUAGE = "Locale.Helper.Selected.Language";
+    public static Context setLocale(Context context, String language) {
+        persist(context, language);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return updateResources(context, language);
+        }
+        return updateResourcesLegacy(context, language);
+    }
+    private static void persist(Context context, String language) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(SELECTED_LANGUAGE, language);
+        editor.apply();
+    }
+    @TargetApi(Build.VERSION_CODES.N)
+    private static Context updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Configuration configuration = context.getResources().getConfiguration();
+        configuration.setLocale(locale);
+        configuration.setLayoutDirection(locale);
+        return context.createConfigurationContext(configuration);
+    }
+    @SuppressWarnings("deprecation")
+    private static Context updateResourcesLegacy(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Resources resources = context.getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLayoutDirection(locale);
+        }
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        return context;
     }
 }
