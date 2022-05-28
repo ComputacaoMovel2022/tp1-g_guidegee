82c9d06f92d15ea37795f7de360f36c3174cc281
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2022-05-28 14:55:09 +0100

    added top and bar fragments to some pages

--- ProjetoCM/app/src/main/java/com/example/projetocm/TopBarFragment.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/TopBarFragment.java
@@ -7,9 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
 public class TopBarFragment extends Fragment {
+
+    DAOUser daoUser = new DAOUser();
 
     public TopBarFragment() {
         super(R.layout.top_bar);
@@ -20,13 +28,35 @@ public class TopBarFragment extends Fragment {
         View view = inflater.inflate(R.layout.top_bar,
                 container, false);
         ImageView settingsButton = view.findViewById(R.id.settingsTopBar);
-        settingsButton.setOnClickListener(new View.OnClickListener()
-        {
+        settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 view.getContext().startActivity(new Intent(view.getContext(), Settings.class));
                 getActivity().finish();
+            }
+        });
+
+        ImageView historyButton = view.findViewById(R.id.historyTopBar);
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                daoUser.getDatabaseReference().child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User user = snapshot.getValue(User.class);
+                        if (user.isUserGuide()) {
+                            view.getContext().startActivity(new Intent(view.getContext(), GuideHistory.class));
+                        } else {
+                            view.getContext().startActivity(new Intent(view.getContext(), RefugeeHistory.class));
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
         return view;
