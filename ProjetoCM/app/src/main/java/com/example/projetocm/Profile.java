fdfd9cb48d0bf8f5a0e4388ee506032ae5e207e4
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2022-05-28 20:27:55 +0100

    new changes

--- ProjetoCM/app/src/main/java/com/example/projetocm/Profile.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/Profile.java
@@ -2,7 +2,11 @@ package com.example.projetocm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        ImageView editProfileButton = findViewById(R.id.editProfileButton);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Profile.this, EditProfile.class));
+                finish();
+            }
+        });
+
+        SharedPreferences preferences = getSharedPreferences("userDefinitions", MODE_PRIVATE);
+        String isGuideString = preferences.getString("isGuide", "");
+        if (isGuideString.equals("true")) {
+            editProfileButton.setVisibility(View.VISIBLE);
+        } else {
+            editProfileButton.setVisibility(View.INVISIBLE);
+        }
     }
 }
