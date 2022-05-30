99780b562f70f6671cc1c578c9e74759a5e68e3a
andrelisboa1 <201902856@estudantes.ips.pt>
2022-05-30 01:05:16 +0100

    Fixed the map partially

--- ProjetoCM/app/src/main/java/com/example/projetocm/MapActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/MapActivity.java
@@ -7,9 +7,11 @@ import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.projetocm.databinding.ActivityMapBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -17,18 +19,28 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private double mLat, mLong;
+    private ActivityMapBinding binding;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
         mLat = getIntent().getDoubleExtra("myLat", 0.0);
         mLong = getIntent().getDoubleExtra("myLong", 0.0);
+
+        binding = ActivityMapBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        GoogleMap mMap = googleMap;
+        mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng pos = new LatLng(mLat, mLong);
