ac6e774e731af3769a1646626d5d2840bdf20bd5
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2022-05-30 04:04:32 +0100

    asdasdasd

--- ProjetoCM/app/src/main/java/com/example/projetocm/MapActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/MapActivity.java
@@ -1,10 +1,12 @@
 package com.example.projetocm;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.projetocm.databinding.ActivityMapBinding;
@@ -16,6 +18,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.maps.android.SphericalUtil;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -27,11 +30,13 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     private GoogleMap mMap;
     private double distance;
     private LatLng mPos, gPos;
+    private String guideKey;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        guideKey = getIntent().getStringExtra("guideKey");
         mLat = getIntent().getDoubleExtra("myLat", 0.0);
         mLong = getIntent().getDoubleExtra("myLong", 0.0);
         gLat = getIntent().getDoubleExtra("gLat", 0.0);
@@ -63,9 +68,26 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         if (distance > gRadius) {
             colorFillLayout = R.color.invalid_map_circle_fill;
             colorStrokeLayout = R.color.invalid_map_circle_border;
+            Button reviewList = findViewById(R.id.requestButton);
+
+            reviewList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(MapActivity.this, GuideListActivity.class));
+                }
+            });
         } else {
             colorFillLayout = R.color.valid_map_circle_fill;
             colorStrokeLayout = R.color.valid_map_circle_border;
+
+            Button requestButton = findViewById(R.id.requestButton);
+            requestButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    (new DAOUser()).addUserRequestList(guideKey, FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    startActivity(new Intent(MapActivity.this, GuideListActivity.class));
+                }
+            });
         }
         mMap.addMarker(new MarkerOptions().position(mPos).title("You are here"));
         mMap.addCircle(
