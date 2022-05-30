9839dbb05a24d466d3ee707b81d4c21b8ae216eb
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2022-05-30 03:19:38 +0100

    done

--- ProjetoCM/app/src/main/java/com/example/projetocm/MapActivity.java
+++ ProjetoCM/app/src/main/java/com/example/projetocm/MapActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.projetocm.databinding.ActivityMapBinding;
+import com.example.projetocm.databinding.ActivityMapGreenBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,6 +16,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.SphericalUtil;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,6 +25,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     private ActivityMapBinding binding;
     private GoogleMap mMap;
+    private double distance;
+    private LatLng mPos, gPos;
 
 
     @Override
@@ -33,9 +37,17 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         gLat = getIntent().getDoubleExtra("gLat", 0.0);
         gLong = getIntent().getDoubleExtra("gLong", 0.0);
         gRadius = getIntent().getIntExtra("gRadius", 0);
+        mPos = new LatLng(mLat, mLong);
+        gPos = new LatLng(gLat, gLong);
+        distance = SphericalUtil.computeDistanceBetween(mPos, gPos);
+        if (distance > gRadius) {
+            binding = ActivityMapBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+        } else {
+            ActivityMapGreenBinding bindingGreen = ActivityMapGreenBinding.inflate(getLayoutInflater());
+            setContentView(bindingGreen.getRoot());
+        }
 
-        binding = ActivityMapBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -47,15 +59,21 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng mPos = new LatLng(mLat, mLong), gPos = new LatLng(gLat, gLong);
+        int colorFillLayout, colorStrokeLayout;
+        if (distance > gRadius) {
+            colorFillLayout = R.color.invalid_map_circle_fill;
+            colorStrokeLayout = R.color.invalid_map_circle_border;
+        } else {
+            colorFillLayout = R.color.valid_map_circle_fill;
+            colorStrokeLayout = R.color.valid_map_circle_border;
+        }
         mMap.addMarker(new MarkerOptions().position(mPos).title("You are here"));
         mMap.addCircle(
                 new CircleOptions()
                         .center(gPos)
                         .radius(gRadius)
-                        .fillColor(getColor(R.color.invalid_map_circle_fill))
-                        .strokeColor(getColor(R.color.invalid_map_circle_border))
+                        .fillColor(getColor(colorFillLayout))
+                        .strokeColor(getColor(colorStrokeLayout))
         );
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mPos, 10.0f));
